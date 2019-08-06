c556f49ed6d77b1ec90430292dff6a6e2795c3d4
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-08-06 18:45:29 -0300

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -5,31 +5,82 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain. """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """ Append a new value as well as the last blockchain value to the blockchain
 
     Arguments:
         :transaction_amount: The amount that should be added. 
         :last_transaction: The last blockchain transaction (defail[1]).
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_innput():
+def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float."""
     return float(input('Your transaction amount please:'))
 
 
-tx_amount = get_user_innput()
-add_value(tx_amount)
+def get_user_choice():
+    """ Gets user choice and returns it. """
+    user_input = input('Your choice: ')
+    return user_input
 
-tx_amount = get_user_innput()
-add_value(tx_amount, get_last_blockchain_value())
+def print_blockchain_elements():
+    """ Outputs all blocks of the blockchain. """
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+    else:
+        print('_' * 20)
 
-tx_amount = get_user_innput()
-add_value(tx_amount, get_last_blockchain_value())
+def verify_chain():
+    """ Verify the current blockchain and returns True if it's valid and False otherwise. """
+    is_valid = True
 
-print(blockchain)
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+     return is_valid
+
+waiting_for_input = True
+
+# A while loop for the user input interface
+# It's a loop that exits once waiting_for_input becomes False or when break is called
+while waiting_for_input:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        waiting_for_input = False
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    else:
+        print('Input was invalid, please pick a value from the list!')
+    if not verify_chain():
+        print('Invalid blockchain!')
+        break
+else:
+    print('User Left!')    
+
+print('\nDone!')
