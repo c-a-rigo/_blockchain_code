4d4576ff62857e2b6fc40ce2bd7180af1ab5ba4e
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-08-13 20:27:49 -0300

    Update hash_util.py

--- hash_util.py
+++ hash_util.py
@@ -6,4 +6,6 @@ def hash_string_256(string):
 
 
 def hash_block(block):
-    return hash_string_256(json.dumps(block, sort_keys=True).encode())
+    hashable_lock = block.__dict__.copy()
+    hashable_lock['transactions'] = [tx.to_ordered_dict() for tx in hashable_lock['transactions']]
+    return hash_string_256(json.dumps(hashable_lock, sort_keys=True).encode())
