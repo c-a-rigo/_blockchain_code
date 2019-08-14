f3a451089a1858f7486bcda13b4ee2aff8c02fe2
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-08-14 19:28:30 -0300

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -16,14 +16,17 @@ class Wallet:
         self.public_key = public_key
 
     def save_keys(self):
+       
         if self.public_key != None and self.private_key != None:
             try:
                 with open('wallet.txt', mode='w') as f:
                     f.write(self.public_key)
                     f.write('\n')
                     f.write(self.private_key)
+                return True
             except (IOError, IndexError):
                 print('Saving wallet failed...')
+                return False
 
     def load_keys(self):
         try:
@@ -33,8 +36,10 @@ class Wallet:
                 private_key = keys[1]
                 self.public_key = public_key
                 self.private_key = private_key
+            return True
         except (IOError, IndexError):
             print('Loading wallet failed...')
+            return False
 
     def generate_keys(self):
         private_key = RSA.generate(1024, Crypto.Random.new().read)
