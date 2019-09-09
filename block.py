5928d2126dc77071b4c71ccd24148ff4a708defb
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-09-09 20:39:51 -0300

    Update block.py

--- block.py
+++ block.py
@@ -2,6 +2,7 @@ from time import time
 
 from utility.printable import Printable
 
+
 class Block(Printable):
     def __init__(self, index, previous_hash, transactions, proof, time=time()):
         self.index = index
@@ -9,4 +10,3 @@ class Block(Printable):
         self.timestamp = time
         self.transactions = transactions
         self.proof = proof
-
